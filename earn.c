f78222706b3c9bb93f51db17e3e563e8dff753bb
Alex Warren <Alex.Warren@lakros.com>
2017-08-23 13:20:27 +1200

    Testing my gitflow again. Adding comments to earn.c.

---
+++ earn.c
@@ -0,0 +1,20 @@
+// nothing script, largely for testing git.
+// hence adding in this comment.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+        if ( argc !=2 )
+        {
+                printf("usage: %s filename hours", argv[0] );
+        }
+        else
+        {
+                int w = 28;
+                int h = atoi(argv[1]);
+                int e = (w * h);
+                printf("%d", e);
+        }
+}
