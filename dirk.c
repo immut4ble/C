895e2e534a26ed6ca6e82469044744c4c21cb994
Alex Warren <Alex.Warren@lakros.com>
2017-08-24 11:54:48 +1200

    Fixing my indentation, adding a title comment.

--- dirk.c
+++ dirk.c
@@ -2,54 +2,54 @@
 #include <sys/types.h>
 #include <dirent.h>
 
-int
-main (void)
+// Returns the contents of the drectories i use at work.
+int main (void)
 {
-  DIR *reg;
-  DIR *tr;
-  DIR *dfx;
-  struct dirent *ep;
+        DIR *reg;
+        DIR *tr;
+        DIR *dfx;
+        struct dirent *ep;
 
-  // Clearing the console.
-  int system(const char *command);
-  system("cls");
+    // Clearing the console.
+    int system(const char *command);
+    system("cls");
 
-  tr = opendir ("D:\\SVN\\OnlineTransactions");
-  puts("*-- Online Transactions --*");
-  if (tr != NULL)
+    tr = opendir ("D:\\SVN\\OnlineTransactions");
+    puts("*-- Online Transactions --*");
+    if (tr != NULL)
     {
-      while (ep = readdir (tr))
-        puts (ep->d_name);
-      (void) closedir (tr);
+        while (ep = readdir (tr))
+            puts (ep->d_name);
+            (void) closedir (tr);
     }
-  else
+    else
+        perror ("Couldn't open the directory");
+
+    puts("\n");
+    reg = opendir ("D:\\SVN\\OnlineRegistration");
+    puts("*-- Online Registration --*");
+    if (reg != NULL)
+    {
+        while (ep = readdir (reg))
+            puts (ep->d_name);
+            (void) closedir (reg);
+    }
+    else
     perror ("Couldn't open the directory");
 
-  puts("\n");
-  reg = opendir ("D:\\SVN\\OnlineRegistration");
-  puts("*-- Online Registration --*");
-  if (reg != NULL)
+    puts("\n");
+    dfx = opendir ("D:\\SVN\\DFX");
+    puts("*---------- DFX ----------*");
+    if (dfx != NULL)
     {
-      while (ep = readdir (reg))
-        puts (ep->d_name);
-      (void) closedir (reg);
+        while (ep = readdir (dfx))
+            puts (ep->d_name);
+            (void) closedir (dfx);
     }
-  else
-    perror ("Couldn't open the directory");
+    else
+        perror ("Couldn't open the directory");
 
-  puts("\n");
-  dfx = opendir ("D:\\SVN\\DFX");
-  puts("*---------- DFX ----------*");
-  if (dfx != NULL)
-    {
-      while (ep = readdir (dfx))
-        puts (ep->d_name);
-      (void) closedir (dfx);
-    }
-  else
-    perror ("Couldn't open the directory");
-
-  puts("\n");
-  puts("\n");
-  return 0;
+    puts("\n");
+    puts("\n");
+    return 0;
 }
